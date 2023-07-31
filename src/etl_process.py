f495b3a745a3faddc9a11ff62c1530bde5dca9b9
unknown <Administrator@RU-M-0142.jedi.tom-tailor.com>
2023-07-31 22:03:14 +0300

    added generators

--- src/etl_process.py
+++ src/etl_process.py
@@ -39,26 +39,21 @@ def load_data_with_logging(csv_files):
     for csv_file in csv_files:
         table_name = csv_file.split('/')[-1].split('.')[0].upper()
         table_name = table_name.lower()
-        df = pd.read_csv(csv_file, sep=';', encoding='cp866', index_col = 0)
-        print("table name:", table_name)
+        df = pd.read_csv(csv_file, sep=';', encoding='cp866')
+        print(table_name)
         print("Data from CSV file:")
         print(df)
 
         try:
             # Запись данных в таблицу в схеме DS
             df.to_sql(table_name, engine, schema='ds', if_exists='replace', index=False)
-            for index, row in df.iterrows():
-                row_data = row.astype(object).replace(np.nan, 'None').to_dict()
-                # Логирование каждой строки в таблицу logs.logs_table
-                log_data = {
-                    'log_time': str(datetime.datetime.now()),
-                    'name_of_table':str(table_name),
-                    'log_data': json.dumps(row_data),
-                    'status': 'success'
-                }
-                log_entry = pd.DataFrame([log_data])
-                log_entry.to_sql('logs', engine, schema='logs', if_exists='append', index=False)
-
+            log = pd.DataFrame({
+                'log_time': [str(datetime.datetime.now()) for i in range(len(df))],
+                'name_of_table': [table_name for i in range(len(df))],
+                'log_data': df.apply(lambda x: json.dumps({i: j for i, j in x.astype(object).replace(np.nan, 'None').items()}), axis=1),
+                'status': ['success' for i in range(len(df))]
+            })
+            log.to_sql('logs', engine, schema='logs', if_exists='append', index=False)
         except Exception as e:
             # Логирование ошибки загрузки в таблицу logs.logs_table
             log_data = {
